dc07ff82ce2a49d40e4028813622279f0931d891
safilo <wulinqing@hotmail.com>
2016-07-13 02:05:42 +0800

    吴林青-第一周作业

--- 第十组/第一次作业/吴林青/第一周/mkdir.js
+++ 第十组/第一次作业/吴林青/第一周/mkdir.js
@@ -1,13 +1,13 @@
-var fs = require("fs");
-
 function makeP(path) {
     var arr = path.split("/");
     var n = 1;
     fs.mkdir(arr.slice(0, n).join("/"), function(err) {
         console.log(n, arr.slice(0, n).join("/"));
-        if (n < arr.length) {
-            n++;
-            return fs.mkdir(arr.slice(0, n).join("/"), arguments.callee);
+        if(!err) {
+            if (n < arr.length) {
+                n++;
+                return fs.mkdir(arr.slice(0, n).join("/"), arguments.callee);
+            }
         }
     });
 }
